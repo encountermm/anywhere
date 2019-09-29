ce605c825b6785fd53ed59cd85309e0574be8c97
encountermm <1120010842@qq.com>
2019-09-30 01:11:14 +0800

    完结

--- src/app.js
+++ src/app.js
@@ -1,35 +1,25 @@
 const http = require('http')
 const chalk = require('chalk')
 const path = require('path')
-const fs = require('fs')
 const conf = require('./config/defaultConfig')
+const route = require('./helper/route')
+const openUrl = require('./helper/openUrl')
+class Server {
+  constructor(config) {
+    this.conf = Object.assign({}, conf, config)
+  }
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(this.conf.root, req.url)
+      route(req, res, filePath, this.conf)
+    })
 
-const server = http.createServer((req, res) => {
-  const url = req.url
-  const filePath = path.join(conf.root, url)
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      res.statusCode = 404
-      res.setHeader('Content-Type', 'text/plain')
-      res.end(`${filePath} is not a directory or file`)
-      return
-    }
-    if (stats.isFile()) {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/plain')
-      fs.createReadStream(filePath).pipe(res)
-    } else if (stats.isDirectory()) {
-      fs.readdir(filePath, (err, files) => {
-        if (err) return
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(files.join(','))
-      })
-    }
-  })
-})
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`
+      console.info(`Server started at ${chalk.green(addr)}`)
+      openUrl(addr)
+    })
+  }
+}
 
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`
-  console.info(`Server started at ${chalk.green(addr)}`)
-})
+module.exports = Server
